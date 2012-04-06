3d99767b51adccde9c95250f087e622bb7fa7aac
John Cockrum <johncockrum1@yahoo.com>
2012-04-06 12:55:30 -0700

    connecting mme to db

--- makemeelvis/includes/_addemail.php
+++ makemeelvis/includes/_addemail.php
@@ -7,7 +7,7 @@
 <?php include('includes/header.php'); ?> 
 
 <?php
-  $dbc = mysqli_connect('http://184.169.130.102/index.php', 'root', '1q2w3e4r', 'elvis_store') or die('Error connecting to MySQL server.');
+  $dbc = mysqli_connect('http://184.169.130.102/_addemail.php', 'root', '1q2w3e4r', 'elvis_store') or die('Error connecting to MySQL server.');
 
   $first_name = $_POST['firstname'];
   $last_name = $_POST['lastname'];
