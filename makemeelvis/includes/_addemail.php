a46d0b83a3e3e331d232ac08f99699a19de1f536
John Cockrum <johncockrum1@yahoo.com>
2012-04-06 13:32:40 -0700

    connecting mme to db

--- makemeelvis/includes/_addemail.php
+++ makemeelvis/includes/_addemail.php
@@ -23,10 +23,10 @@
         $last_name = $_POST['lastname'];
         $email = $_POST['email'];
 
-        echo "INSERT INTO $tbl_name (first_name, last_name, email)  VALUES ('$first_name', '$last_name', '$email')";
+        echo "INSERT INTO $tbl_name (first_name, last_name, email)  VALUES ('$first_name', '$last_name', '$email') <br />";
 
-        $query = "INSERT INTO $tbl_name (first_name, last_name, email)  VALUES ('$first_name', '$last_name', '$email')";
-        mysqli_query($query) or die('Error querying database.');
+       
+        mysqli_query( "INSERT INTO $tbl_name (first_name, last_name, email)  VALUES ('$first_name', '$last_name', '$email');") or die('Error querying database.');
         
         echo 'Customer added.';
         mysqli_close($dbc);
