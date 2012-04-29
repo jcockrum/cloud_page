d4a9ad040e2d40a506b21e66c3c4b2a39acccfda
Jennifer Rene <jrene1@mail.csuchico.edu>
2012-04-28 20:11:16 -0700

    still working on drop down list box for appts

--- project/includes/drop_down.php
+++ project/includes/drop_down.php
@@ -5,54 +5,55 @@
 		print_r($_SESSION);
 		echo "<br />end debug=-=-=-=-=-=-=-=-=-=<br />";
 		
-		// var's Defintion:
-        $_SESSION['err_msg']    ="";                    // Clear error messgae
-		$host		        	="127.0.0.1"; 	        // Host name
-		$db_usr		        	="root"; 	        	// Mysql username
-		$db_pwd		        	="1q2w3e4r"; 	        // Mysql password
-		$db_name	        	="project"; 	        // Database name
-		$tbl_name	       		="Dates"; 	       	    // Table name	
+		function datesArray() 
+		{
+			// var's Defintion:
+			$_SESSION['err_msg']    ="";                    // Clear error messgae
+			$host		        	="127.0.0.1"; 	        // Host name
+			$db_usr		        	="root"; 	        	// Mysql username
+			$db_pwd		        	="1q2w3e4r"; 	        // Mysql password
+			$db_name	        	="project"; 	        // Database name
+			$tbl_name	       		="Dates"; 	       	    // Table name	
 
-        if (isset($_SESSION['username'])) 
-        {           
-                // Connect to the database
-                $dbc = mysqli_connect($host, $db_usr, $db_pwd, $db_name) or die("connection failure with " . $host . " -> " . $dbname);
-                echo "<p>Connected to:" . $host . " -> " . $db_name . " </p><br />"; //debuging
-                $query = "SELECT * FROM $tbl_name";
-                $data = mysqli_query($dbc, $query) or die("<p>Query failure:" . $query . " </p><br />");
-                echo "<p>Sending query: " . $query . " </p><br />"; //debuging
-                //get number of rows returned
-                $num_datas = $data->num_rows;
-                
-				if( $num_datas > 0)//will not make a table without records
-                { 
-					$options = array(  ); 
-					$cntr = 0 ; 
-					while( $row = $data->fetch_assoc() ) //loop to show each records
-                        {   //extract row -- this will make a $row['firstname'] to just a variable $firstname only
-                            extract($row);
-                            $tmp =  " {$Cal_Date} {$At_time} <br /> " ;  //debuging  
-							$options[$cntr++] = $tmp; 
-							echo $tmp ; 
-						}
+			if (isset($_SESSION['username'])) 
+			{           
+					// Connect to the database
+					$dbc = mysqli_connect($host, $db_usr, $db_pwd, $db_name) or die("connection failure with " . $host . " -> " . $dbname);
+					echo "<p>Connected to:" . $host . " -> " . $db_name . " </p><br />"; //debuging
+					$query = "SELECT * FROM $tbl_name";
+					$data = mysqli_query($dbc, $query) or die("<p>Query failure:" . $query . " </p><br />");
+					echo "<p>Sending query: " . $query . " </p><br />"; //debuging
+					//get number of rows returned
+					$num_datas = $data->num_rows; 
+					if( $num_datas > 0)//will not make a table without records
+					{ 
+						$options = array(  ); 
+						$cntr = 0 ; 
+						while( $row = $data->fetch_assoc() ) //loop to show each records
+							{   //extract row -- this will make a $row['firstname'] to just a variable $firstname only
+								extract($row);
+								$tmp =  " {$Cal_Date} {$At_time} <br /> " ;  //debuging  
+								$options[$cntr++] = $tmp; 
+								echo $tmp ; 
+							}				   
+					} else {
+							echo "No records found.";
+					}
+					// Cleanup
+					$data->free();
+					$dbc->close();      
+			} else { 
+					$_SESSION['err_msg'] = 'Sorry, you must be loged in to complete this action.';  
+					 header("location: ../_php_fail.php"); // comment this line to activate debugging
+			}
+			Return $option ; 
+		}
 
-				   
-                } else {
-                        echo "No records found.";
-                }
-				
-                // Cleanup
-                $data->free();
-                $dbc->close();      
-        } else { 
-                $_SESSION['err_msg'] = 'Sorry, you must be loged in to complete this action.';  
-                 header("location: ../_php_fail.php"); // comment this line to activate debugging
-        }
-
-
-/* 
 	function dropdown( $name, array $options, $selected=null )
 	{
+		//Fill the options array 
+		$options = datesArray() ; 
+		
 		// begin the select 
 		$dropdown = '<select name="'.$name.'" id="'.$name.'">'."\n";
 
@@ -72,21 +73,21 @@
 
 		// and return the completed dropdown 
 		return $dropdown;
-	} */ 
+	} 
 
 ?> 
 <form>
 
 <?php
 
-/*
+
 $name = 'my_dropdown';
-// $options = array( 'dingo', 'wombat', 'kangaroo' );
+$options = array( );
 $selected = 1;
 
 echo dropdown( $name, $options, $selected );
 
-*/ 
+ 
 
 ?>
 </form> 
