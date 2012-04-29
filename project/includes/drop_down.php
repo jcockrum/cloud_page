3cc2f655e0d56d8bbe779b156eabd025b678bc13
Jennifer Rene <jrene1@mail.csuchico.edu>
2012-04-28 18:48:58 -0700

    still working on drop down list box for appts

--- project/includes/drop_down.php
+++ project/includes/drop_down.php
@@ -27,15 +27,19 @@
 				if( $num_datas > 0)//will not make a table without records
                 { 
                    
+					$options = array(  );
+					//foreach ($arr as &$value) 
+					//{
+						//$value = $value * 2;
+					//}
+				   
+				   
 					while( $row = $data->fetch_assoc() ) //loop to show each records
                         {       //extract row -- this will make a $row['firstname'] to just a variable $firstname only
                                 extract($row);
-                               
-                                $tmp =  " {$Cal_Date} {$At_time} <br /> " ;  //debuging  
-								echo $tmp ; 
-        
-                                
-                        }
+                               // $tmp =  " {$Cal_Date} {$At_time} <br /> " ;  //debuging  
+								//echo $tmp ; 
+						}
 
 				   
                 } else {
