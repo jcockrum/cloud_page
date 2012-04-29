0b107c49d88cbfadfbd004fbbf01232f25e3e1f6
Jennifer Rene <jrene1@mail.csuchico.edu>
2012-04-28 17:48:00 -0700

    still working on drop down list box for appts

--- project/includes/drop_down.php
+++ project/includes/drop_down.php
@@ -42,45 +42,48 @@
                 header("location: ../_php_fail.php"); // comment this line to activate debugging
         }
 
-
+/* 
 	function dropdown( $name, array $options, $selected=null )
 	{
-		/*** begin the select ***/
+		// begin the select 
 		$dropdown = '<select name="'.$name.'" id="'.$name.'">'."\n";
 
 		$selected = $selected;
-		/*** loop over the options ***/
+		// loop over the options 
 		foreach( $options as $key=>$option )
 		{
-			/*** assign a selected value ***/
+			/ assign a selected value /
 			$select = $selected==$key ? ' selected' : null;
 
-			/*** add each option to the dropdown ***/
+			/ add each option to the dropdown /
 			$dropdown .= '<option value="'.$key.'"'.$select.'>'.$option.'</option>'."\n";
 		}
 
-		/*** close the select ***/
+		// close the select 
 		$dropdown .= '</select>'."\n";
 
-		/*** and return the completed dropdown ***/
+		// and return the completed dropdown 
 		return $dropdown;
-	}
+	} */ 
 
 ?> 
 <form>
 
 <?php
+
+/*
 $name = 'my_dropdown';
 // $options = array( 'dingo', 'wombat', 'kangaroo' );
 $selected = 1;
 
 echo dropdown( $name, $options, $selected );
 
+*/ 
+
 ?>
 </form> 
 
 
-?> 
 
   
 
