6e2c9d84b5f07bab4b465c2b3129a0c0d411b31d
Jennifer Rene <jrene1@mail.csuchico.edu>
2012-05-02 21:02:38 -0700

    testing drop down list script into appointments

--- project/includes/_appoinments_form.php
+++ project/includes/_appoinments_form.php
@@ -4,7 +4,7 @@
  
  <div class="field">
         <label for="Cal_date">Service Date:</label><br />
-        <?php include_once("location: ./drop_down.php"); ?>
+        <?php include_once(drop_down.php); ?>
   </div>
   
   <div class="field">
